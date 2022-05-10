db2d451b1f38865de8d666d9a359307cb0ca0184
lanmower <almagestfraternite@gmail.com>
2022-05-10 20:55:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,11 @@ const child = require('child_process').exec(
 );
 
 
+async function toArray(iterable) {
+  const result = []
+  for await (const data of iterable) result.push(data)
+  return result
+}
 let mod = 0;
 const tunnels = {};
 const agent = new http.Agent(
@@ -66,26 +71,24 @@ var proxy = httpProxy.createProxyServer({
   timeout: 360000
 });
 const doServer = async function (req, res) {
-        console.log(req.headers);
-  if(!req.headers.host) return;
+  if (!req.headers.host) return;
   const split = req.headers.host.split('.');
-  const publicKey = await getKey(split[split.length-3]);
-  let target = '';
+  const publicKey = await getKey(split[split.length - 3]);
   if (!tunnels[publicKey]) {
     const port = 1337 + ((mod++) % 1000);
     try {
-        var server = net.createServer(function (local) {
-          const socket = node.connect(publicKey);
-          socket.write('http');
-          pump(local, socket, local);
-        });
-        server.listen(port, "127.0.0.1");
-        tunnels[publicKey] = port;
-        target = 'http://127.0.0.1:' + port;
-      } catch(e) {
-        console.trace(e);
-        console.error(e);
-      }
+      var server = net.createServer(function (local) {
+        const socket = node.connect(publicKey);
+        socket.write('http');
+        pump(local, socket, local);
+      });
+      server.listen(port, "127.0.0.1");
+      tunnels[publicKey] = port;
+      target = 'http://127.0.0.1:' + port;
+    } catch (e) {
+      console.trace(e);
+      console.error(e);
+    }
   } else {
     target = 'http://127.0.0.1:' + tunnels[publicKey]
   }
@@ -99,32 +102,75 @@ const doServer = async function (req, res) {
 }
 var server = http.createServer(doServer);
 server.listen(80);
-const getKey = (name)=>{
+const known = {};
+const getKey = async (name) => {
   let publicKey;
-  let decoded;
-  try {
-	decoded = b32.decode.asBytes(name.toUpperCase())} catch (e) {
-          console.error(e)
+  let decoded = '';
+  try { decoded = b32.decode.asBytes(name.toUpperCase()) } catch (e) {
+    console.error(e)
   }
   if (decoded.length == 32) publicKey = Buffer.from(decoded);
+  else {
+    if(!known[name]) {
+      try {
+        known[name] = JSON.parse(fs.readFileSync('known/'+name));
+        known[name].key = Buffer.from(known[name].key, 'hex');
+      } catch(e) {}
+    }
+    if(known[name] && new Date().getTime() - known[name].last < 15*60*1000) {
+	    return known[name].key;
+    }
+    const target = DHT.hash(Buffer.from(name));
+    const result = await toArray(node.lookup(target));
+    if (result.length > 0) {
+      if(known[name]) {
+        let foundknown;
+        for(res of result) {
+          for(peer of res.peers) {
+              if(known[name.key] == peer.publicKey) {
+                known[name].last = new Date().getTime();
+                foundknown = true;
+                known[name].key =known[name].key.toString('hex');
+                fs.writeFileSync('known/'+name, JSON.stringify(known[name]));
+              }
+          }
+        }
+        if(!foundknown && new Date().getTime() - known[name].last > 60*60^1000*24) {
+          known[name].key = Buffer.from(result[0].peers[0].publicKey);
+          known[name].last = new Date().getTime();
+          known[name].key =known[name].key.toString('hex');
+          fs.writeFileSync('known/'+name, JSON.stringify(known[name]));
+        }
+      } else {
+        known[name]={};
+        known[name].key = Buffer.from(result[0].peers[0].publicKey);
+        known[name].last = new Date().getTime();
+        known[name].key =known[name].key.toString('hex');
+        fs.writeFileSync('known/'+name, JSON.stringify(known[name]));
+      }
+      if(known[name]) return known[name].key;
+    } else {
+      console.log('no results');
+    }
+  }
   return publicKey;
 }
 
-net.createServer(function(local) {
-    local.once("data", function(data) {
-        const server = sni(data);
-        if(server) {
-          const split = server.split('.');
-          if(split[split.length-3]) {
-            const publicKey = getKey(split[split.length-3]);
-                  if(!publicKey) return;
-            const socket = node.connect(publicKey);
-            console.log(socket);
-            socket.write('https');
-            socket.write(data);
-            pump(local, socket, local);
-          }
-        }
-    });
+net.createServer(function (local) {
+  local.once("data", async function (data) {
+    const server = sni(data);
+    if (server) {
+      const split = server.split('.');
+      if (split[split.length - 3]) {
+        const publicKey = await getKey(split[split.length - 3]);
+        if (!publicKey) return;
+        console.log(publicKey);
+        const socket = node.connect(publicKey);
+        socket.write('https');
+        socket.write(data);
+        pump(local, socket, local);
+      }
+    }
+  });
 }).listen(443);
-
+console.log('created server')
